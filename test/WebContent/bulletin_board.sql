bb67bf96b4531ed2ca9dae46794031636a532fb4
leesehee <dydtp0615@naver.com>
2020-07-31 13:19:00 +0900

    테이블 까지 불틴보드 테이블에 다 모아놨습니다. 진짜 최종완성본입니다,

--- test/WebContent/bulletin_board.sql
+++ test/WebContent/bulletin_board.sql
@@ -1,3 +1,18 @@
+CREATE TABLE member
+(
+    id           VARCHAR2(100)    NOT NULL, 
+    pwd          VARCHAR2(100)    NOT NULL, 
+    nick         VARCHAR2(100)    NOT NULL, 
+    email        VARCHAR2(100)    NOT NULL, 
+    profile      VARCHAR2(100)    NULL, 
+    regdate      DATE             NOT NULL, 
+    quiz         NUMBER           NOT NULL, 
+    quizcheck    VARCHAR2(100)    NOT NULL, 
+    CONSTRAINT MEMBER_PK PRIMARY KEY (id, nick, email)
+);
+
+CREATE SEQUENCE MEMBER_seq;
+
 CREATE TABLE bulletin_board
 (
     num        NUMBER          NOT NULL, 
@@ -9,5 +24,20 @@ CREATE TABLE bulletin_board
     kinds      VARCHAR2(20)    NOT NULL, 
     CONSTRAINT BULLETIN_BOARD_PK PRIMARY KEY (num)
 );
+CREATE SEQUENCE BULLETIN_BOARD_SEQ;
 
-CREATE SEQUENCE bulletin_board_seq;
+CREATE TABLE board_comment
+(
+    num         NUMBER          NOT NULL, 
+    name        VARCHAR2(40)    NOT NULL, 
+    content     CLOB            NOT NULL, 
+    regdate     DATE            NOT NULL, 
+    boardnum    NUMBER          NOT NULL, 
+    CONSTRAINT BOARD_COMMENT_PK PRIMARY KEY (num)
+);
+
+ALTER TABLE board_comment
+    ADD CONSTRAINT FK_board_comment_boardnum_bull FOREIGN KEY (boardnum)
+        REFERENCES bulletin_board (num) ON DELETE CASCADE;
+
+CREATE SEQUENCE BOARD_COMMENT_SEQ;
